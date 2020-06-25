251e44178be90b7e6cb33cf6d36737737d0c00f3
lKaza <rodrigoguzman1202@gmail.com>
2020-06-25 00:42:26 -0400

    Added AIction movement Scheduler Combat Targetting y mas

---
+++ Assets/Scripts/Combat/Fighter.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using RPG.Core;
+using RPG.Movement;
+using UnityEngine;
+
+namespace RPG.Combat
+{
+    public class Fighter : MonoBehaviour,IAction
+        {
+            [SerializeField] float weaponRange =2f;
+            Transform target;
+            private void Update() {
+                if(target != null){
+
+                    GetComponent<Mover>().MoveTo(target.position);
+                    float distance = Vector3.Distance(transform.position,target.position);
+                    if(distance <=weaponRange){                   
+                    GetComponent<Mover>().Disengage();
+                    }
+
+                }
+            }
+
+            public void Attack(CombatTarget combatTarget){
+                GetComponent<Scheduler>().StartAction(this);
+                target = combatTarget.transform;
+            }
+            public void Disengage(){
+                if(target == null) return;
+            
+                target = null;
+            }
+        }
+
+}
---
+++ Assets/Scripts/Movement/Mover.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using RPG.Core;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace RPG.Movement{
+
+public class Mover : MonoBehaviour, IAction
+{
+    [SerializeField] Transform target;
+    Animator myAnim;
+    NavMeshAgent navMesh;
+
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMesh = GetComponent<NavMeshAgent>();
+        myAnim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        Animator();
+    
+    }
+
+   
+   public void StartMoveAction(Vector3 destination){
+     GetComponent<Scheduler>().StartAction(this);
+       MoveTo(destination);
+   }
+
+    public void MoveTo(Vector3 destination)
+    {
+        navMesh.isStopped = false;
+        navMesh.SetDestination(destination);
+    }
+
+    private void Animator()
+    {
+       Vector3 velocity = navMesh.velocity;
+       Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+       float speed = localVelocity.z;
+        myAnim.SetFloat("forwardSpeed",speed);
+    }
+    public void Disengage(){
+       
+        navMesh.isStopped = true;
+    }
+    
+}
+}
