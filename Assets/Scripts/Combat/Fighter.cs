9d0d62fcb600a2780642950aa1483e54691c42f0
lKaza <rodrigoguzman1202@gmail.com>
2020-06-25 01:29:26 -0400

    Animator through code

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -8,21 +8,36 @@ namespace RPG.Combat
 {
     public class Fighter : MonoBehaviour,IAction
         {
+            Animator myAnim;
             [SerializeField] float weaponRange =2f;
             Transform target;
+        private void Start() {
+            myAnim = GetComponent<Animator>();
+        }
+
             private void Update() {
                 if(target != null){
 
                     GetComponent<Mover>().MoveTo(target.position);
                     float distance = Vector3.Distance(transform.position,target.position);
-                    if(distance <=weaponRange){                   
+                    if(distance <=weaponRange)
+                {
+
                     GetComponent<Mover>().Disengage();
-                    }
+                    AttackAnimation();
 
                 }
+
+            }
             }
 
-            public void Attack(CombatTarget combatTarget){
+        private void AttackAnimation()
+        {
+            myAnim.SetTrigger("Attack");
+        }
+
+        public void Attack(CombatTarget combatTarget){
+                
                 GetComponent<Scheduler>().StartAction(this);
                 target = combatTarget.transform;
             }
@@ -30,6 +45,11 @@ namespace RPG.Combat
                 if(target == null) return;
             
                 target = null;
+            }
+
+            //Animation event
+            void Hit(){
+            
             }
         }
 
