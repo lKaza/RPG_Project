bbbb6a0aa43a27457009bd3d34fe5ae754f8b28e
lKaza <rodrigoguzman1202@gmail.com>
2020-06-25 17:23:05 -0400

    Prefab variant of character as player and enemy small changes between those

---
+++ Assets/Health.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Combat{
+
+
+public class Health : MonoBehaviour
+{
+        [SerializeField] int maxHealth=100;
+        private int currentHealth;
+        // Start is called before the first frame update
+        void Start()
+        {
+            currentHealth = maxHealth;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+        public void TakeDmg(int dmg)
+        {
+            currentHealth = currentHealth - dmg;
+            if(currentHealth<=0){
+                print("me dead");
+            }
+        }
+    }
+}
--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -9,13 +9,22 @@ namespace RPG.Combat
     public class Fighter : MonoBehaviour,IAction
         {
             Animator myAnim;
+            
             [SerializeField] float weaponRange =2f;
+            [SerializeField] float TimeBetweenAttacks = 1f;
+            [SerializeField] int weaponDmg=5;
+            float timeSinceLastAttack = 0;
+
             Transform target;
         private void Start() {
+           
             myAnim = GetComponent<Animator>();
+           
         }
 
             private void Update() {
+                timeSinceLastAttack +=Time.deltaTime;
+
                 if(target != null){
 
                     GetComponent<Mover>().MoveTo(target.position);
@@ -24,16 +33,20 @@ namespace RPG.Combat
                 {
 
                     GetComponent<Mover>().Disengage();
-                    AttackAnimation();
-
+                    if(timeSinceLastAttack>=TimeBetweenAttacks)
+                    {
+                        //Triggers Hit() animation dmg
+                        AttackBehaviour();
+                        timeSinceLastAttack = 0;
+                    }
                 }
-
             }
-            }
+        }
 
-        private void AttackAnimation()
+        private void AttackBehaviour()
         {
             myAnim.SetTrigger("Attack");
+            
         }
 
         public void Attack(CombatTarget combatTarget){
@@ -49,8 +62,7 @@ namespace RPG.Combat
 
             //Animation event
             void Hit(){
-            
-            }
+                    target.GetComponent<Health>().TakeDmg(weaponDmg);
+            }                   
         }
-
 }
