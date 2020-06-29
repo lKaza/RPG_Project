edf7d5943c0b9804fca3821013c83256443522a9
lKaza <rodrigoguzman1202@gmail.com>
2020-06-29 01:19:33 -0400

    draw gizmos for patrol

---
+++ Assets/DrawWaypoint.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Control{
+
+
+public class DrawWaypoint : MonoBehaviour
+{
+    void OnDrawGizmos()
+    {
+        
+        Gizmos.color = Color.red;
+        for(int i=0; i<transform.childCount;i++)
+        {
+           
+            Gizmos.DrawSphere(transform.GetChild(i).position, 0.3f);
+            Gizmos.DrawLine(transform.GetChild(i).position, transform.GetChild(i+1).position);
+
+        }
+    }
+}
+}
