572598d6828c2a645182c0bfede45836a488418e
lKaza <rodrigoguzman1202@gmail.com>
2020-07-04 22:14:12 -0400

    Added new terrain- portals and fadeout/fadein al pasar por los portales

---
+++ Assets/Fader.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.SceneManagement{
+    public class Fader : MonoBehaviour
+        {
+           
+           CanvasGroup canvasGroup;
+           float deltaAlpha;
+           private void Start() {
+               canvasGroup = GetComponent<CanvasGroup>();
+           }
+          
+
+           public IEnumerator FadeOut(float transitionTime)
+           {
+              
+            while(canvasGroup.alpha <1)
+            {
+                deltaAlpha = Time.deltaTime / transitionTime;
+                canvasGroup.alpha += deltaAlpha;
+                yield return null;
+            }
+
+           
+        }
+        public IEnumerator FadeIn(float transitionTime)
+        {
+           
+            while (canvasGroup.alpha > 0)
+            {
+                deltaAlpha = Time.deltaTime / transitionTime;
+                canvasGroup.alpha -= deltaAlpha;
+                yield return null;
+            }
+
+        }
+        }
+
+}
---
+++ Assets/Portal.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.AI;
+
+namespace RPG.SceneManagement{
+
+
+    public class Portal : MonoBehaviour
+    {
+        enum DestinationIdentifier{
+            A,B,C,D,E
+        }
+        [SerializeField] int sceneIndex=0;
+        [SerializeField] Transform spawnPoint;
+        [SerializeField] DestinationIdentifier currentPortal;
+        [SerializeField] DestinationIdentifier destination;
+        [SerializeField] float fadeOutTime = 2f;
+        [SerializeField] float fadeInTime = 2f;
+        [SerializeField] float blackscreenTime = 0.3f;
+
+        // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other) {
+        if(other.tag=="Player"){
+            
+            StartCoroutine(Transition());
+        }
+        
+    }
+        private IEnumerator Transition()
+        {
+            Fader fader = FindObjectOfType<Fader>();
+            
+            DontDestroyOnLoad(this.gameObject);
+            yield return StartCoroutine(fader.FadeOut(fadeInTime));
+            yield return SceneManager.LoadSceneAsync(sceneIndex);
+
+            Portal otherPortal = GetOtherPortal();          
+            UpdatePlayer(otherPortal);
+
+            yield return new WaitForSeconds(blackscreenTime);
+            yield return StartCoroutine(fader.FadeIn(fadeInTime));
+            Destroy(this.gameObject);
+        }
+       
+        private void UpdatePlayer(Portal otherPortal)
+        {
+        GameObject player = GameObject.FindWithTag("Player");
+        player.GetComponent<NavMeshAgent>().Warp(otherPortal.spawnPoint.position);
+        player.transform.rotation = otherPortal.spawnPoint.rotation;
+        }
+
+        private Portal GetOtherPortal()
+        {
+           
+           foreach(Portal p in FindObjectsOfType<Portal>()){
+               if(p.currentPortal == destination){
+                   return p;
+               }
+           }
+            return null;
+        }
+    }
+}
