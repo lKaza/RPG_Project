88fa0584d4093dda298a3c5396c96031b16624ae
lKaza <rodrigoguzman1202@gmail.com>
2020-07-06 15:42:08 -0400

    added saving with dictionary and struct type commented

--- Assets/Scripts/Movement/Mover.cs
+++ Assets/Scripts/Movement/Mover.cs
@@ -60,15 +60,39 @@ public class Mover : MonoBehaviour, IAction ,ISaveable
 
         public object CaptureState()
         {
-           return new SerializableVector3(transform.position);
+            Dictionary< string,object> data = new Dictionary<string, object>();
+            data["position"] = new SerializableVector3(transform.position);
+            data["rotation"] = new SerializableVector3(transform.eulerAngles);
+           return data;
         }
 
         public void RestoreState(object state)
         {
-           SerializableVector3 position = (SerializableVector3)state;
+            Dictionary<string, object> data = (Dictionary<string, object>)state;
            GetComponent<NavMeshAgent>().enabled = false;
-           transform.position = position.ToVector();
+           transform.position = ((SerializableVector3)data["position"]).ToVector();
+           transform.eulerAngles = ((SerializableVector3)data["rotation"]).ToVector();
             GetComponent<NavMeshAgent>().enabled = true;
         }
+        // struct save/load data method
+        /* struct MoverSaveData
+        {
+        [System.Serializable]
+           public SerializableVector3 position;
+           public SerializableVector3 rotation;
+        } 
+        CaptureState(){
+            Mover data = new MoverSaveData();
+            data.position = new SerializableVector3(transform.position);
+            data.rotation = new SerializableVector3(transform.eulerAngles);
+            return data;
+        }
+        RestoreState()
+        MoverSaveData data = (MoverSaveData)state;
+         GetComponent<NavMeshAgent>().enabled = false;
+         transform.position = data.position.ToVector();
+         transform.eulerAngles = data.rotation.ToVector();
+         GetComponent<NavMeshAgent>().enabled = true;
+        */
     }
 }
