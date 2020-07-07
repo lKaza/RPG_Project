80c48ac662a65b2382d30d7c7c37611a125c5c65
lKaza <rodrigoguzman1202@gmail.com>
2020-07-06 20:24:31 -0400

    Serialization/Deserialization methods from scratch

---
+++ Assets/Scripts/Saving/SavingWrapper.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace RPG.Saving{
+    public class SavingWrapper : MonoBehaviour
+    {
+        SavingSystem savingSystem;
+        const string savelocation ="save";
+        // Start is called before the first frame update
+        void Start()
+        {
+            savingSystem = GetComponent<SavingSystem>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if(Input.GetKeyDown(KeyCode.S))
+            {
+                savingSystem.Save(savelocation);
+            }
+            if (Input.GetKeyDown(KeyCode.L))
+            {
+                savingSystem.Load(savelocation);
+            }
+        }
+    }
+
+}
